99947776d7ca7e67d26fc7432023dd62bee62d82
zaltaie <46233852+zaltaie@users.noreply.github.com>
2020-05-02 13:50:00 -0700

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 import sys
-sys.path.insert(1,'/Users/zaid/Downloads')
 from login import user, passw
 from time import sleep
 from selenium.webdriver.common.keys import Keys
@@ -9,7 +8,7 @@ import csv
 
 class TinderBot():
     def __init__(self):
-        self.driver = webdriver.Chrome('/Users/zaid/Downloads/chromedriver')
+        self.driver = webdriver.Chrome()
 
     def login(self):
         self.driver.get('https://tinder.com')
@@ -19,7 +18,7 @@ class TinderBot():
             string = "//button[contains(.,'More Options')]"
             moreops = self.driver.find_element_by_xpath(string)
             moreops.click()
-            sleep(0.25)
+            sleep(1)
             fb_path = '//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[3]/button'
             fb_btn = self.driver.find_element_by_xpath(fb_path)
             fb_btn.click()
@@ -27,3 +26,50 @@ class TinderBot():
             fb_path = '//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button'
             fb_btn = self.driver.find_element_by_xpath(fb_path)
             fb_btn.click()
+
+        # switch to login popup
+        base_window = self.driver.window_handles[0]
+        popup = self.driver.window_handles[1]
+        self.driver.switch_to.window(popup)
+
+        sleep(2)
+
+        email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
+        email_in.send_keys(user)
+
+        pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
+        pw_in.send_keys(passw)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
+        login_btn.click()
+
+        sleep(2)
+        # switch to original window
+
+        self.driver.switch_to.window(base_window)
+
+        sleep(3)
+
+        '''
+        #Optional phone login
+
+        phone_in = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div[2]/div/input')
+        phone_in.send_keys(phone_num)
+
+        continue_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/button')
+        continue_btn.click()
+
+        #Need to input authentication code manually
+        sleep(15)
+        '''
+
+        loc_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        loc_popup.click()
+
+        notif_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        notif_popup.click()
+
+        sleep(10)
+
+        passport_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/button')
+        passport_popup.click()
